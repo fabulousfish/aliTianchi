1353b8f5ca2cb1f30a4b62c90db0208142c89b48
kongxinyu <kongxinyu@baidu.com>
2018-03-16 16:21:28 +0800

    data_clean string to int

--- ad_model/data_helper/data_read.py
+++ ad_model/data_helper/data_read.py
@@ -6,22 +6,27 @@ Date: 2018-3-15
 """
 
 import os
-import numpy
+import re
 import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-data_test_path = os.path.join(current_dir, "../data/test_file")
+data_path = os.path.join(current_dir, "../data/test_file")
 
 def data_read():
     clean_list = [2, 3, 18]
-    data = pd.read_table(data_test_path, header=None, encoding='gb2312', delim_whitespace=True)
+    data = pd.read_table(data_path, header=None, encoding='gb2312', delim_whitespace=True)
     for list_num in clean_list:
         for i in range(len(data[list_num])):
-            data[list_num][i] = data[list_num][i].split(';')
+            try:
+                list_string = re.split('[:,;]', data[list_num][i])
+                data[list_num][i] = list(map(int, list_string))
+            except Exception as e:
+                print(e)
+                print(list_num, i)
     data_list = data.T
     return data_list
 
 
 if __name__ == '__main__':
-    data_read()
+    data_list = data_read()
 
